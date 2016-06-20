3f9bd13587be50deac5b46e5db584889ad45d782
jasonbs10 <jschiller@twitter.com>
2016-06-19 20:30:48 -0700

    Ignore certain fuse ops instead of returning an error (errors cause s… (#7)
    Add OpSetattr to the unsupported list to work with minfs.

--- fuse.go
+++ fuse.go
@@ -234,7 +234,7 @@ func processBuf(b []byte, scope *RequestScope, handler Servlet) (req Request, re
 		if req, resp, err = parseGetattr(b, scope.alloc, scope.conn.proto); handler != nil && err == nil {
 			handleErr = handler.HandleGetattr(req.(*GetattrRequest), resp.(*GetattrResponse))
 		}
-	case OpGetxattr, OpListxattr, OpFlush, OpForget, OpInterrupt, OpDestroy:
+	case OpGetxattr, OpListxattr, OpSetattr, OpFlush, OpForget, OpInterrupt, OpDestroy:
 		req, resp, err = parseUnsupported(b, scope.alloc)
 		handleErr = ENOSYS
 	case OpLookup:
